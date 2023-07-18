ac451b1a20a0e4fdc19f8bd414418590bf029c41
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-07-18 02:27:07 -0500

    Updated array information. Created functional button to change the serviceType variable when ever count changes in the array of abjects called services

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import {useState,useEffect} from 'react';
 
 function App() {
-  //-------------- Declare What Hooks Are Needed
-  
+  //-------------- Declare What Hooks Are Needed: STATE
+     //--Initial variable, functiontochangevariable = useStatehook and  INITIAL VALUE i.e. an empty string as the parameter
   const [serviceType, setServiceType] = useState(``);
-  const[count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+
+  //------{Global Variables that May Affect the State ; in this project, I used services as the global variable}// 
+  // NB: Knowing the type of features needed on the App helps you know what features needs new information uploaded when the App is in use; and then use those features in the useState hook. E.g. In this project, the features that need to be changed are serviceType and images. I used just serviceType to declare the  useState hook.
 
   let services = [
     {
@@ -20,11 +23,11 @@ function App() {
     }, 
     {
       serviceType: `Notary Services`,
-      image:(``),
+      image:(`https://s3-media0.fl.yelpcdn.com/bphoto/DqtJzRkxdlE9hROQ_qsbDg/300s.jpg`),
       }, 
       {
         serviceType: `DJ Services`,
-        image:(``),
+        image:(`https://media.tenor.com/zuYQRDdHhyYAAAAM/dj-neon.gif`),
         },    
    ]
 
@@ -50,7 +53,7 @@ function App() {
       <title>www.sags.com</title>
       <h1> Welcome to Sparkle Access Global Services (SAGS)</h1>
       {/* //NarBar = Foodhandler/Alcohol training course;Food manager Procto Services;Notary Services; DJ services; */}
-      <span> Who we Are </span>
+      <span> Who We Are </span>
       <p> SAGs is a service company providing you educational and entertainment services <br/> Click on the CHOOSE SERVICE button to browse through the different types of services we offer </p>
       <br/> <br/>
       <h2> Type of Service : {serviceType.serviceType} </h2>
@@ -59,12 +62,6 @@ function App() {
       <div>
         <button onClick={changeCount}> CHOOSE SERVICE </button>
       </div>
-      
-
-
-
-
-
     </div>
   );
 }
